5399f762c2fe9ea1afd8b1c5ab16403c5eb5a04d
esneyder <edesalla17@gmail.com>
2014-07-25 18:29:06 -0500

    desarrollo de posts y login del sitio web

--- js/funciones.js
+++ js/funciones.js
@@ -5,10 +5,10 @@ $(document).on('ready',function(){
 		if(switMenu%2==0)
 		{
 			$('.nav-menu').slideToggle("slow");
-			$('header').css({'left':'0','width':'100%'}).addClass('bounceInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+			$('header').removeAttr( 'style' ).addClass('bounceInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 			$(this).removeClass();});
-			$('.nav-menu li ul').css('display','none');
-			$('.contenido').css('padding-left','0');
+			$('.nav-menu li ul').removeAttr( 'style' );
+			$('.contenido').removeAttr( 'style' );
 			switMenu=0;
 		}
 		else
@@ -16,13 +16,74 @@ $(document).on('ready',function(){
 			$('.nav-menu').slideToggle("slow") ;
 			$('header').css({'left':'10.8em','width':'87%'}).addClass('bounceInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 			$(this).removeClass();});
-			$('.nav-menu li ul').css('display','none');
+			$('.nav-menu li ul').removeAttr( 'style' );
 			$('.contenido').css('padding-left','180px');
 		}
+	});
 
+        // activar display del menu
 
+	$(".nav-menu>ul>li").click(function(){
+		var contenido=$(this).find('ul');
+		if($(this).find('ul').length==1)
+		{
+	       // boton atras
+	       $('.nav-menu >ul> li:first-child a').css('display','block').addClass('rotateInUpRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+				$(this).removeClass();});
+	     // activar ul li ul
+	       $(this).find('ul').css('display','block');
+	       // pendiente
+	       $(this).find('ul li:first-child a').html($(this).children('a').text());
 
+	      // --------- activar submenu ---------
+	       $('.nav-menu> ul> li:first-child a').click(function(){
+             $(this).removeAttr( 'style' );
+             $(contenido).fadeOut(300);
+	       });
+		}
+	});
+
+	// ------------activar buscador-----------
+		var contarB=0;
+	$('.search-b').click(function(){
+		contarB++;
+		if (contarB%2==0) {
+
+           $('.txt-buscador').fadeToggle().animate({
+        	 width: "0"
+         }, 1000, "swing");
+                   contarB=0;
+
+		}else{
+
+          $('.txt-buscador').css('display','block').animate({
+        	 width: "200px"
+         }, 1000, "swing");
+
+		}
 
 	});
 
+
+
+ $('.login-button').click(function(){
+      	$('.login').css('display','block').addClass('bounceInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+			 });
+      });
+
+ // hover acciones -- activar div acciones
+ //
+  $('.descripcion').hover(function(){
+  	$('.acciones').css('display','block').addClass('rotateInUpRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+		});
+       });
+
+  // cerrar formulario login
+
+  $('.exit').click(function(){
+  	$('.login').css('display','none');
+  });
+
+
 });
+
--- menu-bonito.php
+++ menu-bonito.php
@@ -6,9 +6,13 @@
          <link type="image/x-icon" href="imagenes/icono.ico" rel="icon" />
 <link rel="stylesheet" href="css/estilo-menu.css">
 <link rel="stylesheet" href="css/animate.css">
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<!-- <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script src="js/funciones.js"></script>
+  -->
+  <script src="js/jquery.js"></script>
+ <script src="js/funciones.js"></script>
+
+
 </head>
 <body>
 	<header>
@@ -46,18 +50,233 @@
                      <li><a href="#"></a></li>
                      <li><a href="#">Desarrollo web</a></li>
                      <li><a href="#">Desarrollo móvil</a></li>
+                     <li><a href="#">Base de datos</a></li>
+                     <li><a href="#">Móvil</a></li>
                 </ul>
               </li>
              <li><a href="#">Cursos</a></li>
-              <li><a href="#">Mas vistos</a></li>
+              <li><a href="#">Mas vistos</a>
+                   <ul>
+                     <li><a href="#"></a></li>
+                     <li><a href="#">Programmer</a></li>
+                     <li><a href="#">PHP</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Tutoriales</a>
+                   <ul>
+                     <li><a href="#"></a></li>
+                     <li><a href="#">Base de datos</a></li>
+                     <li><a href="#">Móvil</a></li>
+                </ul>
+              </li>
              <li><a href="#">Contacto</a></li>
          </ul>
 	</nav>
-    <div class="contenido">
-        <div class="post">
+    <!-- formulario  login -->
+     <div class="login">
+            <h2 class="titulologin">Iniciar sesión</h2>
+            <div class="salirlogin"><a href="#" class="exit">X</a></div>
+            <hr>
+           <div class="redessociales">
+            <div class="loginface">
+            <a href="#" class="f"><img src="imagenes/face_seguir.png" alt="Facebook"><span class="t-loginface">Facebook</span></a>
+          </div>
+          <div class="logingo">
+            <a href="#" class="g"> <img src="imagenes/google_plus.png" alt="Google"><span class="t-logingoogle">Google</span></a>
+
+         </div>
 
         </div>
+  <form action="javascript:val(0)" class="formulariologin" method="post">
+        <label for="usuario" name="usuario" >Usuario:</label><input  name="usuario" type="text" class="txt-login" placeholder="Usuario o Email">
+         <label for="Password" name="password">Password:</label><input name="password" type="password" class="txt-login" placeholder="Password">
+           <a href="#" class="ocontrasena">¿Olvidaste la contraseña?</a>
+          <div class="btn-formlogin">
 
+          <input type="submit" value="Ingresar" class="btn-login">
+       </div>
+       <div class="registrarme">
+        <p class="crearcuenta">No tengo aún cuenta.</p>
+           <input type="submit" value="Registrarme" class="btn-registrar">
+       </div>
+       </form>
+
+
+</div>
+<!-- posts-> contenidos publicaciones -->
+
+    <div class="contenido">
+
+
+             <article class="post">
+            <div class="descripcion">
+
+
+                   <div class="cabtitulo">
+
+                        <h1 class="titulo">
+                              Programador
+                        </h1>
+                        <a class="tag" href="#">Desarrollo</a>
+                    </div>
+
+
+                    <figure class="imagen">
+                        <img src="imagenes/posts.png" />
+                    </figure>
+
+                    <div class="detalles">
+
+                             <div class="resumen">
+                              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ea deserunt, id harum libero dolore distinctio.
+                             </div>
+
+                        <div class="detinfo">
+
+
+
+                                      <strong>Por </strong><a href="#" class="autor">Edwin Esneyder</a>
+
+                                    <div class="fecha">hace <strong>20</strong> min</div>
+                        </div>
+                </div>
+
+            </div>
+
+
+            <div class="acciones">
+                <div class="votos">
+                    <a class="up" href="#"></a>
+                    <span class="total">100<span>
+                    <a class="down" href="#"></a>
+                </div>
+
+                <div class="social">
+                    <a class="megusta" href="#"><img src="imagenes/like.png" alt=""></a>
+                    <a class="compface" href="#"><img src="imagenes/facebook_square.png" alt=""></a>
+                    <a class="comptwitter" href="#"><img src="imagenes/twitter_square.png" alt=""></a>
+                    <a class="comppinteres" href="#"><img src="imagenes/linkedin_square.png" alt=""></a>
+
+            </div>
+              <div class="datos">
+                 <a class="comentarios" href="#">10</a>
+               <a class="estrellita" href="#"></a>
+                </div>
+            </div>
+            </article>
+
+             <article class="post">
+            <div class="descripcion">
+
+
+                   <div class="cabtitulo">
+
+                        <h1 class="titulo">
+                              Programador
+                        </h1>
+                        <a class="tag" href="#">Desarrollo</a>
+                    </div>
+
+
+                    <figure class="imagen">
+                        <img src="imagenes/posts.png" />
+                    </figure>
+
+                    <div class="detalles">
+
+                             <div class="resumen">
+                              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ea deserunt, id harum libero dolore distinctio.
+                             </div>
+
+                        <div class="detinfo">
+
+
+
+                                      <strong>Por </strong><a href="#" class="autor">Edwin Esneyder</a>
+
+                                    <div class="fecha">hace <strong>20</strong> min</div>
+                        </div>
+                </div>
+
+            </div>
+
+
+            <div class="acciones">
+                <div class="votos">
+                    <a class="up" href="#"></a>
+                    <span class="total">100<span>
+                    <a class="down" href="#"></a>
+                </div>
+
+                <div class="social">
+                    <a class="megusta" href="#"><img src="imagenes/like.png" alt=""></a>
+                    <a class="compface" href="#"><img src="imagenes/facebook_square.png" alt=""></a>
+                    <a class="comptwitter" href="#"><img src="imagenes/twitter_square.png" alt=""></a>
+                    <a class="comppinteres" href="#"><img src="imagenes/linkedin_square.png" alt=""></a>
+
+            </div>
+              <div class="datos">
+                 <a class="comentarios" href="#">10</a>
+               <a class="estrellita" href="#"></a>
+                </div>
+            </div>
+            </article>
+             <article class="post">
+            <div class="descripcion">
+
+
+                   <div class="cabtitulo">
+
+                        <h1 class="titulo">
+                              Programador
+                        </h1>
+                        <a class="tag" href="#">Desarrollo</a>
+                    </div>
+
+
+                    <figure class="imagen">
+                        <img src="imagenes/posts.png" />
+                    </figure>
+
+                    <div class="detalles">
+
+                             <div class="resumen">
+                              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ea deserunt, id harum libero dolore distinctio.
+                             </div>
+
+                        <div class="detinfo">
+
+
+
+                                      <strong>Por </strong><a href="#" class="autor">Edwin Esneyder</a>
+
+                                    <div class="fecha">hace <strong>20</strong> min</div>
+                        </div>
+                </div>
+
+            </div>
+
+
+            <div class="acciones">
+                <div class="votos">
+                    <a class="up" href="#"></a>
+                    <span class="total">100<span>
+                    <a class="down" href="#"></a>
+                </div>
+
+                <div class="social">
+                    <a class="megusta" href="#"><img src="imagenes/like.png" alt=""></a>
+                    <a class="compface" href="#"><img src="imagenes/facebook_square.png" alt=""></a>
+                    <a class="comptwitter" href="#"><img src="imagenes/twitter_square.png" alt=""></a>
+                    <a class="comppinteres" href="#"><img src="imagenes/linkedin_square.png" alt=""></a>
+
+            </div>
+              <div class="datos">
+                 <a class="comentarios" href="#">10</a>
+               <a class="estrellita" href="#"></a>
+                </div>
+            </div>
+            </article>
     </div>
     </body>
 </html>
